616e979c684f983a20675408aa1b8bd756014e47
franconick <nickalamil2@gmail.com>
2025-11-12 18:20:00 -0700

    Probably works again

--- boondoggle-portfolio/src/components/Nav.jsx
+++ boondoggle-portfolio/src/components/Nav.jsx
@@ -1,14 +1,33 @@
-function Nav () {
-    return(
-      <nav className="navbar">
-        <div className="nav-content">
-          <div className="nav-logo">Logo</div>
-          <ul className="nav-links">
-            <li><a href="#home">Thumbnails</a></li>
-            <li><a href="#about">Banners</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </div>
-      </nav>
-    );
+import './Nav.css'
+import logo from '../assets/BDlogoSMALL.png'
+
+function Nav() {
+  const handleLogoClick = () => {
+    // Navigate to home - can be updated when routing is set up
+    window.location.href = '/'
+  }
+
+  const handleLinkClick = (page) => {
+    // Navigate to page - can be updated when routing is set up
+    // For now, just prevent default and log
+    console.log(`Navigate to ${page}`)
+  }
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo" onClick={handleLogoClick} style={{ cursor: 'pointer' }}>
+        <img src={logo} alt="Boondoggle Logo" />
+      </div>
+      <div className="navbar-links">
+        <a href="#" className="navbar-link" onClick={(e) => { e.preventDefault(); handleLinkClick('thumbnails'); }}>Thumbnails</a>
+        <a href="#" className="navbar-link" onClick={(e) => { e.preventDefault(); handleLinkClick('thumbnails'); }}>Banners</a>
+        <a href="#" className="navbar-link" onClick={(e) => { e.preventDefault(); handleLinkClick('other'); }}>Other</a>
+        <a href="#" className="navbar-link" onClick={(e) => { e.preventDefault(); handleLinkClick('info'); }}>Info</a>
+        <a href="#" className="navbar-link" onClick={(e) => { e.preventDefault(); handleLinkClick('contact'); }}>Contact</a>
+      </div>
+    </nav>
+  )
 }
+
+export default Nav
+
--- boondoggle-portfolio/src/pages/Home.jsx
+++ boondoggle-portfolio/src/pages/Home.jsx
@@ -3,14 +3,17 @@ import backgroundImage from '../assets/BDbackgroundHOMEnooverlay.png'
 import boonChar from '../assets/booncharBLUE.png'
 import overlayImage from '../assets/BDbackgroundHOMEoverlay.png'
 import LinkButton from '../components/link-button/LinkButton.jsx'
+import Nav from '../components/Nav.jsx'
 
 function Home(){
     return( 
         
         <div className="home-container">
+            <Nav></Nav>
             <div className="background-image">
                 <img src={backgroundImage} alt="background" />
             </div>
+
             <div className="character-image">
                 <img src={boonChar} alt="boondoggle character" />
             </div>
